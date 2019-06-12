5311afbfe06cba8337099ba783cf7b4add663a69
steph <superhappyfunsexytime@gmail.com>
2019-06-11 23:22:25 -0400

    Add level script to track progression

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,16 +7,20 @@ public class Ball : MonoBehaviour {
 	// configuration paramerters
 	[SerializeField] Paddle paddle1;
 	[SerializeField] Vector2 ballVelocity = new Vector2(0f, 10f);
-
-	bool gameHasStarted = false;
+	[SerializeField] AudioClip[] ballSounds;
 
 	// state
 	Vector2 paddleToBallVector;
+	bool gameHasStarted = false;
+
+	// Cached component references
+	AudioSource myAudioSource;
 
     // Start is called before the first frame update
     void Start() {
 		paddleToBallVector = transform.position - paddle1.transform.position;
-    }
+		myAudioSource = GetComponent<AudioSource>();
+	}
 
     // Update is called once per frame
     void Update() {
@@ -36,5 +40,12 @@ public class Ball : MonoBehaviour {
 	private void LockBallToPaddle() {
 		Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
 		transform.position = paddlePos + paddleToBallVector;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		if (gameHasStarted) {
+			AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+			myAudioSource.PlayOneShot(clip);
+		}
 	}
 }
--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -3,7 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour {
+
+	[SerializeField] AudioClip breakSound;
+
+	// cached refernece
+	Level level;
+
+	void Start() {
+		level = FindObjectOfType<Level>();
+		level.CountBreakableBlocks();
+	}
+
 	private void OnCollisionEnter2D(Collision2D collision) {
+		DestroyBlock();
+	}
+
+	private void DestroyBlock() {
+		AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+		level.BlockDestroyed();
 		Destroy(gameObject);
 	}
 }
